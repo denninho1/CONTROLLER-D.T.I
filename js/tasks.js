50bc2c8edd27c11f621877b6a1ba29e28567da4c
Dennis Cesar <dennis.dennis2012@gmail.com>
2026-01-09 17:10:22 -0300

    limpa os input do formulário

--- js/tasks.js
+++ js/tasks.js
@@ -16,31 +16,33 @@ btnOpenCreateTask.addEventListener('click', () => {
 
 btnClosedCreateTask.addEventListener('click', () => {
     toggleFormCreateTask();
+    cleanTask();
 });
 
+// Variaveis para criar nova tarefa e <ul>
+const btnCreateTask = document.getElementById('btn_createNewTask');
+const listPendencias = document.getElementById('listPendencias');
 
-// criar pendencia
-const btnCreateTask = document.getElementById('btn_createNewTask')
-const listPendencias = document.getElementById('listPendencias')
-
+//Evento de click no botão criar tarefa
 btnCreateTask.addEventListener('click', function (e) {
     e.preventDefault()
     createNewTask();
+    cleanTask();
 })
 
-
+// criar pendencia
 function createNewTask () {
     // Captura dos valores
-    const loja = document.getElementById('taskLoja').value
-    const pendencia = document.getElementById('taskPendencia').value
-    const email = document.getElementById('taskEmail').value
-    const data = document.getElementById('taskDate').value
-    const status = document.getElementById('taskStatus');
-    const statusValue = status.value;
-    const statusText = status.options[status.selectedIndex].text;
+    let loja = document.getElementById('taskLoja').value;
+    let pendencia = document.getElementById('taskPendencia').value;
+    let email = document.getElementById('taskEmail').value;
+    let data = document.getElementById('taskDate').value;
+    let status = document.getElementById('taskStatus');
+    let statusValue = status.value;
+    let statusText = status.options[status.selectedIndex].text;
 
     if (!loja || !pendencia || !email || !statusValue) {
-        alert('Preencha os campos obrigatórios!')
+        alert('Preencha os campos obrigatórios!');
         return
     }
 
@@ -50,8 +52,8 @@ function createNewTask () {
         : 'Não informada'
 
     // Criação do item
-    const li = document.createElement('li')
-    li.classList.add('box_pend')
+    const li = document.createElement('li');
+    li.classList.add('box_pend');
 
     li.innerHTML = `
         <div class="box_pend_info">
@@ -78,12 +80,12 @@ function createNewTask () {
 
     // Botão excluir
     li.querySelector('.delete').addEventListener('click', () => {
-        li.remove()
+        li.remove();
     })
 
-    listPendencias.appendChild(li)
+    listPendencias.appendChild(li);
     toggleFormCreateTask ();
-    switchStatus(li, statusValue)
+    switchStatus(li, statusValue);
 }
 
 
@@ -97,6 +99,18 @@ function switchStatus (box, selectValue) {
         box.style.backgroundColor = '#fffb00ff';
     } else if (selectValue[4]) {
         box.style.backgroundColor = '#008cffff';
+    } else {
+        alert('erro');
+        return
     }
+};
+
+//Função para preencher o formulario automaticamente
+function cleanTask () {
+    document.getElementById('taskLoja').value = '';
+    document.getElementById('taskPendencia').value = '';
+    document.getElementById('taskEmail').value = '';
+    document.getElementById('taskDate').value = '';
+    document.getElementById('taskStatus').value = '';
 };
 
