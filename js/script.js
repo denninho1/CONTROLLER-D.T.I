9f6142101a595a0df4463c64a9056e6ba80b075c
Dennis Cesar <dennis.dennis2012@gmail.com>
2026-03-30 22:34:51 -0300

    evento para o botão de filtro

--- js/script.js
+++ js/script.js
@@ -29,3 +29,29 @@ function toggleNavbar() {
     navbar.classList.toggle('active');
 }
 
+
+/* Eventos para o botão de filtrar */
+const filterContainer = document.querySelector('.filter_container');
+const filterBtn = document.querySelector('.filter_btn');
+const arrowIcon = filterBtn.querySelector('i');
+
+filterBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    filterContainer.classList.toggle('active');
+
+    arrowIcon.classList.toggle('fa-caret-down');
+    arrowIcon.classList.toggle('fa-caret-up');
+});
+
+document.addEventListener('click', () => {
+    filterContainer.classList.remove('active')
+
+    arrowIcon.classList.remove('fa-caret-up');
+    arrowIcon.classList.add('fa-caret-down');
+
+})
+
+filterContainer.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
